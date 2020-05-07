d2916eb62b52256f4b2bb7a06f88917d7c5d1953
anshuUnity <64951054+anshuUnity@users.noreply.github.com>
2020-05-07 22:09:39 +0530

    FirstCommit
    Basic Project added

---
+++ 2D Car racing/Assets/scripts/CarController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float carSpeed;
+    Vector3 position;
+    public float maxPos = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+
+        position.x = Mathf.Clamp(position.x, -2.0f, 2.0f);
+        transform.position = position;
+    }
+}
---
+++ 2D Car racing/Assets/scripts/EnemyCarMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCarMove : MonoBehaviour
+{
+
+    public float speedEnemyCar = 5f; // speedwith which enemy car move
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 1, 0) * speedEnemyCar * Time.deltaTime);
+    }
+}
